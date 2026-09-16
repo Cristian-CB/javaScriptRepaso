cdd15ea2467c67214f9a2d602186bbd6083a2409
Tu Nombre <TuNombre@email.com>
2026-09-16 13:21:31 +0200

    variables, sentencias simples

---
+++ script.js
@@ -0,0 +1,59 @@
+ 
+ /* 
+ atajo 
+
+ctrl shift a 
+
+
+ */
+ 
+ 
+ // comentario simple
+ 
+
+ // variables var/let/ const
+
+ let a,b,c
+ a = 1 
+ 
+ b=2
+// const varible q no cambia, nombre:mayuscula
+const PI = Math.PI
+console.log(PI)
+
+ // imprimir en consola 
+ console.log(a);
+ 
+// IMPRIMIR TIPOS
+console.log(typeof a)
+//operadores 
+
+console.log(a + b)
+console.log(a - b)
+console.log( a /b)
+console.log(a%b)
+console.log(a ** b)
+console.log(b ++ ) // postIncremento (primero funcion)
+console.log(++b ) // preIncremento (primero suma)
+console.log();
+
+
+
+// operadores logicos 
+
+
+
+&& = and
+
+console.log(1 == "1"); //true
+console.log(1 ==="1"); //false
+console.log();
+/* 
+!== estricto
+!= no estricto 
+
+
+*/
+// !== != 
+sdhg
+
---
+++ sentencias.js
@@ -0,0 +1,38 @@
+let a = 5 , b= 10 , c="10"
+
+//==
+if (b==c) {
+    console.log(" b = c");
+    
+    
+}
+else {
+    console.log("aqui no entra");
+    
+}
+
+
+
+// ===
+if (b===c) {
+    console.log(" no entra");
+    
+    
+}
+else {
+    console.log("aqui  entra");
+    
+}
+
+// switch
+switch (a) {
+    case 5:
+        
+        console.log("entraste");
+        
+        
+        break;
+
+    default:
+        break;
+}
